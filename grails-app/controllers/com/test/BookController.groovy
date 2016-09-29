d23c0a7c7ec052ce41c31f407e59fec04781c862
Fabian Rupp <fabian@eevol.lu>
2016-09-29 12:04:58 +0200

    Project

---
+++ grails-app/controllers/com/test/BookController.groovy
@@ -0,0 +1,7 @@
+package com.test
+
+class BookController {
+
+    static scaffold = Book
+
+}
---
+++ grails-app/domain/com/test/Book.groovy
@@ -0,0 +1,10 @@
+package com.test
+
+class Book {
+
+	String author
+	String title
+
+    static constraints = {
+    }
+}
